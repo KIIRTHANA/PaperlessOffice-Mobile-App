f92c075f5bd5ceed7e09d3d1383a55246b662770
Manjot Sidhu <manjot.techie@gmail.com>
2020-03-10 21:24:48 +0530

    Initial Android Project Setup * Android X * Splash Screen

---
+++ app/src/main/java/hexageeks/daftar/views/SplashScreen.java
@@ -0,0 +1,17 @@
+package hexageeks.daftar.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hexageeks.daftar.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}

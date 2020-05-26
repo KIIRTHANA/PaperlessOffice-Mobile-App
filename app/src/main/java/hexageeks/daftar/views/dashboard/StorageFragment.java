55dfd2d157c3346a468b1567ed081d259398ee56
kirthanaa <kirthana.s@btech.christuniversity.in>
2020-05-26 23:11:08 +0530

    SwipeToRefresh added

--- app/src/main/java/hexageeks/daftar/views/dashboard/StorageFragment.java
+++ app/src/main/java/hexageeks/daftar/views/dashboard/StorageFragment.java
@@ -2,20 +2,20 @@ package hexageeks.daftar.views.dashboard;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.sax.RootElement;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import hexageeks.daftar.R;
 import hexageeks.daftar.backend.DataProvider;
 import hexageeks.daftar.models.StorageItem;
@@ -25,6 +25,8 @@ public class StorageFragment extends Fragment {
     private Snackbar snackbar;
     private RecyclerView storageRecyclerView;
     private ExtendedFloatingActionButton extendedFloatingActionButton;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,9 +46,30 @@ public class StorageFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 openUploadActivity();
+
             }
         });
 
+        // SwipetoRefresh
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                loadDataToRecyclerView();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeContainer.setRefreshing(false);
+                    }
+                }, 3000);
+            }
+        });
+        //Refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_purple,
+                android.R.color.holo_red_dark,
+                android.R.color.holo_red_light);
 
         loadDataToRecyclerView();
 
@@ -56,24 +79,23 @@ public class StorageFragment extends Fragment {
     void loadDataToRecyclerView() {
         // Data gets loaded
         DataProvider.getInstance().getStorageData(getActivity(), new DataProvider.OnResponse<StorageItem[]>() {
-                @Override
-                public void execute(StorageItem[] data) {
-                    RecyclerView.Adapter storageAdapter = new StorageAdapter(data);
-                    storageRecyclerView.setAdapter(storageAdapter);
+                    @Override
+                    public void execute(StorageItem[] data) {
+                        RecyclerView.Adapter storageAdapter = new StorageAdapter(data);
+                        storageRecyclerView.setAdapter(storageAdapter);
 
-                    if (snackbar.isShown())
-                        snackbar.dismiss();
+                        if (snackbar.isShown())
+                            snackbar.dismiss();
+                    }
                 }
-            }
 
         );
     }
 
-    public void openUploadActivity(){
+    public void openUploadActivity() {
         Intent myIntent = new Intent(StorageFragment.this.getActivity(), UploadFiles.class);
         startActivity(myIntent);
     }
-
 
 
 }
